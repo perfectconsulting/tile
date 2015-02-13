caeeebfcb9dd24626cb88f81060f9189cbf63879
Steven James <steven@perfectconsulting.co.uk>
2015-02-13 15:30:08 +0000

    Create tile2.h

---
+++ tile2.h
@@ -0,0 +1,15 @@
+typedef unsigned short word;
+typedef signed short sword;
+typedef unsigned char byte;
+typedef unsigned long dword;
+typedef signed long sdword;
+
+
+#define VM_MEMORY_SIZE			0xffff
+#define VM_STACK_GAP			0x40
+
+#define VM_STATE_EXECUTE		0
+#define VM_STATE_HAULT			1
+
+#define VM_STACK_PROTECTION
+
